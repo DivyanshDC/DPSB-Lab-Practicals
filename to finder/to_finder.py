3ac78491e4fc0421f749c8881e0e83782209dedd
DivyanshDC <Divyanshchhablani00@gmail.com>
2017-11-08 12:22:39 +0530

    Fixed detecting word inside other words
    The old code gave wrong output. For eg. "To TO tO Tomorrow TOMORROW" gave 5 as number of "to"s.

--- to finder/to_finder.py
+++ to finder/to_finder.py
@@ -1,18 +1,27 @@
-'''
-Question: How many 'to' are there in a sentence provided by user entered in a text file.
-'''
 y=0
-fobj=open("tofinder.txt",'r')
-if not fobj:
-    print "file doesn't exist"
-else:
+try:
+    fobj=open("tofinder.txt",'r')
     a=fobj.readline()
-    x=len(a)
-    for i in range(x):
-        if a[i]=='t' or a[i]=='T':
-            if a[i+1]=='o' or a[i+1]=='O':
-                y=y+1
-
-print "Search completed."
-fobj.close()
-print 'Number of "to" are : ',y
+    while a:
+        print a
+        x=len(a)
+        for i in range(x):
+            if a[i].upper()=='T':
+                if a[i+1].upper()=='O':
+                    if (a[i-1] ==" ") or (a[i-1] == "\n"):
+                        try:
+                            if (a[i+2] == " ") or (a[i+2] == "\n"):
+                                y+=1
+                        except IndexError:
+                            y+=1
+                            pass
+        print y
+        a=fobj.readline()
+    print "Search completed."
+    fobj.close()
+except IOError:
+    print "file doesn't exist"
+    pass
+except EOFError:
+    pass
+print 'Number of "to" are : ', y
